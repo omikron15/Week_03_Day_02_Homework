d5cc72464631b830d39dd75de5d9e39bddcf4cd5
omikron15 <omikron15@hotmail.co.uk>
2018-04-10 16:33:49 +0100

    Find by name method working?

--- Space_Cowboys.rb
+++ Space_Cowboys.rb
@@ -33,23 +33,38 @@ class SpaceCowboy
     @id = result[0]["id"].to_i
   end
 
-  # def delete()
-  # db = PG.connect({dbname: "space_cowboys", host: "localhost"})
-  #
-  # end
+  def delete()
+    db = PG.connect({dbname: "space_cowboys", host: "localhost"})
 
-  # def update()
-  # db = PG.connect({dbname: "space_cowboys", host: "localhost"})
-  #
-  # end
-  #
+    sql = "DELETE FROM cowboys WHERE id = $1"
+
+    values = [@id]
+
+    db.prepare("Delete", sql)
+    db.exec_prepared("Delete", values)
+    db.close
+  end
+
+  def update()
+    db = PG.connect({dbname: "space_cowboys", host: "localhost"})
+
+    sql = "UPDATE cowboys SET (name, bounty_value, homeworld, last_known_location) =
+    ($1, $2, $3, $4) WHERE id = $5"
+
+    values = [@name, @bounty_value, @homeworld, @last_known_location, @id]
+
+    db.prepare("Update", sql)
+    db.exec_prepared("Update", values)
+    db.close()
+  end
+
   def self.all
-  db = PG.connect({dbname: "space_cowboys", host: "localhost"})
-  sql = "SELECT * FROM cowboys"
-  db.prepare("Select", sql)
-  results = db.exec_prepared("Select")
-  db.close
-  return results.map {|result| SpaceCowboy.new (result)}
+    db = PG.connect({dbname: "space_cowboys", host: "localhost"})
+    sql = "SELECT * FROM cowboys"
+    db.prepare("Select", sql)
+    results = db.exec_prepared("Select")
+    db.close
+    return results.map {|result| SpaceCowboy.new (result)}
   end
 
   def self.delete_all
@@ -58,6 +73,28 @@ class SpaceCowboy
     db.prepare("Delete_all", sql)
     db.exec_prepared("Delete_all")
     db.close
+  end
+
+  # Implement a find_by_name method that returns one instance of your
+  # class when a name is passed in, or nil if no instance is found.
+  # Note: this will probably be another class method
+  # self.find_by_name(name) (do you have to use a map method if it's
+  # a single result?
+
+  def self.find_by_name(name)
+      db = PG.connect({dbname: "space_cowboys", host: "localhost"})
+      sql = "SELECT * from cowboys WHERE name = $1"
+      values = [name]
+      db.prepare("Find", sql)
+      results = db.exec_prepared("Find", values)
+      db.close
+
+      if results.count == 0
+        return nil
+      else
+        return results[0]
+      end
+
   end
 
 
